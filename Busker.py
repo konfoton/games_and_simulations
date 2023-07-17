48e4d6e01a8c4eac132db11fbd19cc06df7bfc10
konfoton <127990628+konfoton@users.noreply.github.com>
2023-07-17 17:37:49 +0100

    Add files via upload

---
+++ Busker.py
@@ -0,0 +1,253 @@
+import pygame
+import sys
+import time
+pygame.init()
+# Basic information about game
+WINDOW = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption("busker")
+timer = pygame.time.Clock()
+tile = 50
+FPS = 60
+run = True
+alive = False
+current_state = "main"
+text = pygame.font.Font(None, 100)
+text_number = pygame.font.Font(None, 150)
+start_surface = text.render("START", True, "white")
+start_rect = start_surface.get_rect(center=(500, 300))
+levels_surface = text.render("LEVELS", True, "white")
+levels_rect = levels_surface.get_rect(center=(500, 400))
+music_surface = text.render("MUSIC", True, "white")
+music_rect = music_surface.get_rect(center=(500, 500))
+quit_surface = text.render("QUIT", True, "white")
+quit_rect = quit_surface.get_rect(center=(500, 700))
+one_surface = text_number.render("1", True, "white")
+one_rect = one_surface.get_rect(center=(250, 400))
+two_surface = text_number.render("2", True, "white")
+two_rect = two_surface.get_rect(center=(500, 400))
+three_surface = text_number.render("3", True, "white")
+three_rect = three_surface.get_rect(center=(750, 400))
+data = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+data1 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+data2 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+data3 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+
+class World:
+    def __init__(self, data):
+        self.tiles = []
+        self.tiles_collisions = []
+        self.tile_die_or_win = []
+
+        for index_row, row in enumerate(data):
+            for index_title, tile_type in enumerate(row):
+                if tile_type == 0:
+                    air = pygame.Surface((tile, tile))
+                    air.fill((0, 0, 189))
+                    rect = air.get_rect()
+                    rect.x = index_title * tile
+                    rect.y = index_row * tile
+                    self.tiles.append((air, rect))
+                if tile_type == 1:
+                    dirt = pygame.Surface((tile, tile))
+                    dirt.fill((50, 0, 0))
+                    rect = dirt.get_rect()
+                    rect.x = index_title * tile
+                    rect.y = index_row * tile
+                    self.tiles.append((dirt, rect))
+                    self.tiles_collisions.append((dirt, rect))
+                if tile_type == 2:
+                    lava = pygame.Surface((50, 20))
+                    lava.fill("Red")
+                    rect = lava.get_rect()
+                    rect.x = index_title * tile
+                    rect.y = index_row * tile + 30
+                    self.tile_die_or_win.append(rect)
+                    self.tiles.append((lava, rect))
+                if tile_type == 3:
+                    door = pygame.Surface((50, 100))
+                    door.fill("brown")
+                    rect = door.get_rect(midleft=(index_title * tile, index_row * tile))
+                    self.tile_die_or_win.append(rect)
+                    self.tiles.append((door, rect))
+
+    def draw(self):
+        for tile in self.tiles:
+            WINDOW.blit(tile[0], tile[1])
+
+
+class Player:
+    def __init__(self):
+        self.body = pygame.Surface((tile, 2 * tile))
+        self.body.fill((103, 68, 189))
+        self.rect = self.body.get_rect()
+        self.rect.x = 0
+        self.rect.y = 850
+        self.ground = True
+        self.dy = 0
+
+    def move_and_collisions(self):
+        dx = 0
+        self.dy += 5
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            dx += 10
+        if keys[pygame.K_a]:
+            dx -= 10
+        if keys[pygame.K_SPACE] and self.ground is True:
+            self.dy -= 40
+            self.ground = False
+
+        for collision in world.tiles_collisions:
+            if collision[1].colliderect(self.rect.x, self.rect.y + self.dy, 50, 100):
+                if self.dy >= 0:
+                    self.dy = 0
+                    self.ground = True
+                elif self.dy < 0:
+                    self.dy = 0
+            if collision[1].colliderect(self.rect.x + dx, self.rect.y, 50, 100):
+                dx = 0
+        self.rect.x += dx
+        self.rect.y += self.dy
+        for collision in world.tile_die_or_win:
+            if collision.colliderect(self.rect):
+                global alive
+                alive = False
+
+    def draw(self):
+        WINDOW.blit(self.body, self.rect)
+
+
+world = World(data1)
+player = Player()
+while run:
+    buttons = pygame.mouse.get_pressed()
+    mouse_pos = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    if alive:
+        WINDOW.fill((0, 0, 189))
+        world.draw()
+        player.move_and_collisions()
+        player.draw()
+    else:
+        if current_state == "main":
+            WINDOW.fill((0, 0, 0))
+            WINDOW.blit(start_surface, start_rect)
+            WINDOW.blit(levels_surface, levels_rect)
+            WINDOW.blit(music_surface, music_rect)
+            if start_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                player.rect.x = 0
+                player.rect.y = 850
+                alive = True
+            if levels_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                current_state = "LEVELS"
+            if music_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                current_state = "MUSIC"
+        if current_state == "LEVELS":
+            WINDOW.fill((0, 0, 0))
+            WINDOW.blit(quit_surface, quit_rect)
+            WINDOW.blit(one_surface, one_rect)
+            WINDOW.blit(two_surface, two_rect)
+            WINDOW.blit(three_surface, three_rect)
+            if quit_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                current_state = "main"
+            if one_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                world = World(data1)
+            if two_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                world = World(data2)
+            if three_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                world = World(data3)
+        if current_state == "MUSIC":
+            WINDOW.fill((0, 0, 0))
+            WINDOW.blit(quit_surface, quit_rect)
+            if quit_rect.collidepoint(mouse_pos) and buttons[0] is True:
+                current_state = "main"
+    timer.tick(FPS)
+    pygame.display.update()
